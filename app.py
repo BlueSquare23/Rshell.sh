6bca47839032015e23b78d7f237cd40dd37687a9
John R <johnlradford23@gmail.com>
2022-06-27 03:58:13 -0400

    Tweak app.py to use platform pkg

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from app import run_app
+import platform
+
+HOSTNAME = platform.uname().node
 
 if __name__ == "__main__":
 	app = run_app()
-	app.run(host="wopaguz.com", debug=True)
+	app.run(host=HOSTNAME, debug=True)
